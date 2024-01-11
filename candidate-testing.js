51e6fe4791d04a61d54d78699bda22130954c302
Travis <azai392@gmail.com>
2024-01-11 01:22:02 -0600

    working

--- candidate-testing.js
+++ candidate-testing.js
@@ -3,7 +3,8 @@ const input = require('readline-sync');
 // TODO 2: modify your quiz app to ask 5 questions //
 
 // TODO 1.1a: Define candidateName // 
-let candidateName;
+let candidateName = " ";
+
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
 let question;
 let correctAnswer;
@@ -16,8 +17,10 @@ let correctAnswers;
 let candidateAnswers;
 
 
+
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
+  candidateName = input.question("Please enter your name: ");
 
 }
 
