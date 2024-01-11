53fc4d78f3f43031d3ceaf4b77d66069c0a07877
Travis <azai392@gmail.com>
2024-01-11 01:47:30 -0600

    Part 1 Graded Assignment Done!

--- candidate-testing.js
+++ candidate-testing.js
@@ -6,9 +6,9 @@ const input = require('readline-sync');
 let candidateName = ' ';
 
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question;
-let correctAnswer;
-let candidateAnswer;
+let question = "Who was the first American woman in space? ";
+let correctAnswer = "Sally Ride";
+let candidateAnswer = " ";
 
 
 //TODO: Variables for Part 2
@@ -26,14 +26,22 @@ function askForName() {
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-
+  candidateAnswer = input.question(question);
 
 }
+
+
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
 
+if (candidateAnswer === correctAnswer) {
+  console.log("That is correct! Good job!");
+
+} else {
+  console.log("Wrong Answer, Try Again.");
+}
 
 
   let grade;  //TODO 3.2 use this variable to calculate the candidates score.
@@ -45,7 +53,7 @@ function gradeQuiz(candidateAnswers) {
 function runProgram() {
   askForName();
   // TODO 1.1c: Greet candidate using their name //
-   console.log("Hello, " + candidateName + " !");
+   console.log("Hello,",candidateName + "!");
   askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
