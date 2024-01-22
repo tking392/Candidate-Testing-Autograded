12339015bd58ee3acb1c471d5e2cc88570e4adb5
Travis <azai392@gmail.com>
2024-01-22 15:19:38 -0600

    Part 2 working

--- candidate-testing.js
+++ candidate-testing.js
@@ -12,9 +12,21 @@ let candidateAnswer = " ";
 
 
 //TODO: Variables for Part 2
-let questions;
-let correctAnswers;
-let candidateAnswers;
+let questions = [
+  "Who was the first American woman in space? ", 
+  "True or false: 5 kilometer == 5000 meters? ", 
+  "(5 + 3)/2 * 10 = ? ", 
+  "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", 
+  "What is the minimum crew size for the ISS? "
+];
+let correctAnswers = [
+  "Sally Ride", 
+  "true", 
+  "40", 
+  "Trajectory", 
+  3
+];
+let candidateAnswers = [];
 
 
 
@@ -26,9 +38,12 @@ function askForName() {
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-  candidateAnswer = input.question(question);
+  // candidateAnswer = input.question(question); - Part 1 code
 
-}
+  for (let i = 0; i < questions.length; i++) {
+    candidateAnswers.push(input.question(questions[i])); 
+  }
+} 
 
 
 
